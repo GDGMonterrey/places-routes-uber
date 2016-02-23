ff896550ecb4d2ed5cd6a71a5ae805a4fb0b9f19
Joel Humberto Gómez Paredes <elmaildeldezkareid@gmail.com>
2016-02-23 00:56:35 -0600

    Listeners para autocompletado

--- js/app.js
+++ js/app.js
@@ -1,6 +1,41 @@
+var map;
+
 function initMap() {
-	var map = new google.maps.Map(document.getElementById('map'), {
+	
+	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 25.657071, lng: -100.366366},
 		zoom: 13
 	});
+
+	var origenInput = document.getElementById('origen');
+	var destinoInput = document.getElementById('destino');
+
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(origenInput);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinoInput);
+  
+  var autocompleteOrigen = new google.maps.places.Autocomplete(origenInput);
+  autocompleteOrigen.bindTo('bounds', map);
+
+  var autocompleteDestino = new google.maps.places.Autocomplete(destinoInput);
+  autocompleteDestino.bindTo('bounds', map);
+
+  autocompleteOrigen.addListener('place_changed', function() {
+
+    var place = autocompleteOrigen.getPlace();
+    
+    console.log(place);
+
+    var location = place && place.geometry && place.geometry.location;
+    
+  });
+
+  autocompleteDestino.addListener('place_changed', function() {
+    
+    var place = autocompleteDestino.getPlace();
+    
+    console.log(place);
+
+    var location = place && place.geometry && place.geometry.location;
+    
+  });
 }
